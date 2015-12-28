f553158790012fb7b91385cb4ea29ee80361c855
Avery Katko <averykatko@gmail.com>
2015-12-28 03:55:55 -0500

    map names

--- text/map_names.asm
+++ text/map_names.asm
@@ -1,107 +1,107 @@
 MapNames: ; 71473 (1c:5473)
 PalletTownName: ; 71473 (1c:5473)
-	db "PALLET TOWN@"
+	db "HIWIC@"
 ViridianCityName: ; 7147f (1c:547f)
-	db "VIRIDIAN CITY@"
+	db "SINGRENEBURG@"
 PewterCityName: ; 7148d (1c:548d)
-	db "PEWTER CITY@"
+	db "HARBURG@"
 CeruleanCityName: ; 71499 (1c:5499)
-	db "CERULEAN CITY@"
+	db "BLEOBURG@"
 LavenderTownName: ; 714a7 (1c:54a7)
-	db "LAVENDER TOWN@"
+	db "WADWIC@"
 VermilionCityName: ; 714b5 (1c:54b5)
-	db "VERMILION CITY@"
+	db "BASWEBURG@"
 CeladonCityName: ; 714c4 (1c:54c4)
-	db "CELADON CITY@"
+	db "BOGABURG@"
 FuchsiaCityName: ; 714d1 (1c:54d1)
-	db "FUCHSIA CITY@"
+	db "ROSEBURG@"
 CinnabarIslandName: ; 714de (1c:54de)
-	db "CINNABAR ISLAND@"
+	db "RINDIEG@"
 IndigoPlateauName: ; 714ee (1c:54ee)
-	db "INDIGO PLATEAU@"
+	db "CRISTALLAWANG@"
 SaffronCityName: ; 714fd (1c:54fd)
-	db "SAFFRON CITY@"
+	db "CROHBURG@"
 Route1Name: ; 7150a (1c:550a)
-	db "ROUTE 1@"
+	db "WEG 1@"
 Route2Name: ; 71512 (1c:5512)
-	db "ROUTE 2@"
+	db "WEG 2@"
 Route3Name: ; 7151a (1c:551a)
-	db "ROUTE 3@"
+	db "WEG 3@"
 Route4Name: ; 71522 (1c:5522)
-	db "ROUTE 4@"
+	db "WEG 4@"
 Route5Name: ; 7152a (1c:552a)
-	db "ROUTE 5@"
+	db "WEG 5@"
 Route6Name: ; 71532 (1c:5532)
-	db "ROUTE 6@"
+	db "WEG 6@"
 Route7Name: ; 7153a (1c:553a)
-	db "ROUTE 7@"
+	db "WEG 7@"
 Route8Name: ; 71542 (1c:5542)
-	db "ROUTE 8@"
+	db "WEG 8@"
 Route9Name: ; 7154a (1c:554a)
-	db "ROUTE 9@"
+	db "WEG 9@"
 Route10Name: ; 71552 (1c:5552)
-	db "ROUTE 10@"
+	db "WEG 10@"
 Route11Name: ; 7155b (1c:555b)
-	db "ROUTE 11@"
+	db "WEG 11@"
 Route12Name: ; 71564 (1c:5564)
-	db "ROUTE 12@"
+	db "WEG 12@"
 Route13Name: ; 7156d (1c:556d)
-	db "ROUTE 13@"
+	db "WEG 13@"
 Route14Name: ; 71576 (1c:5576)
-	db "ROUTE 14@"
+	db "WEG 14@"
 Route15Name: ; 7157f (1c:557f)
-	db "ROUTE 15@"
+	db "WEG 15@"
 Route16Name: ; 71588 (1c:5588)
-	db "ROUTE 16@"
+	db "WEG 16@"
 Route17Name: ; 71591 (1c:5591)
-	db "ROUTE 17@"
+	db "WEG 17@"
 Route18Name: ; 7159a (1c:559a)
-	db "ROUTE 18@"
+	db "WEG 18@"
 Route19Name: ; 715a3 (1c:55a3)
-	db "SEA ROUTE 19@"
+	db "SÆWEG 19@"
 Route20Name: ; 715b0 (1c:55b0)
-	db "SEA ROUTE 20@"
+	db "SÆWEG 20@"
 Route21Name: ; 715bd (1c:55bd)
-	db "SEA ROUTE 21@"
+	db "SÆWEG 21@"
 Route22Name: ; 715ca (1c:55ca)
-	db "ROUTE 22@"
+	db "WEG 22@"
 Route23Name: ; 715d3 (1c:55d3)
-	db "ROUTE 23@"
+	db "WEG 23@"
 Route24Name: ; 715dc (1c:55dc)
-	db "ROUTE 24@"
+	db "WEG 24@"
 Route25Name: ; 715e5 (1c:55e5)
-	db "ROUTE 25@"
+	db "WEG 25@"
 ViridianForestName: ; 715ee (1c:55ee)
-	db "VIRIDIAN FOREST@"
+	db "SINGRENEWEALD@"
 MountMoonName: ; 715fe (1c:55fe)
-	db "MT.MOON@"
+	db "MONBEORG@"
 RockTunnelName: ; 71606 (1c:5606)
-	db "ROCK TUNNEL@"
+	db "ROCCÞEOTE@"
 SeaCottageName: ; 71612 (1c:5612)
-	db "SEA COTTAGE@"
+	db "SÆCOT@"
 SSAnneName: ; 7161e (1c:561e)
-	db "S.S.ANNE@"
+	db "SANCTA ANNA@"
 PokemonLeagueName: ; 71627 (1c:5627)
-	db "#MON LEAGUE@"
+	db "POCCAÞYRS GEÞOFTA@"
 UndergroundPathName: ; 71633 (1c:5633)
-	db "UNDERGROUND PATH@"
+	db "UNDERGRUND WEG@"
 PokemonTowerName: ; 71644 (1c:5644)
-	db "#MON TOWER@"
+	db "POCCAÞYRS TORR@"
 SeafoamIslandsName: ; 7164f (1c:564f)
-	db "SEAFOAM ISLANDS@"
+	db "SÆFAM IEGE@"
 VictoryRoadName: ; 7165f (1c:565f)
-	db "VICTORY ROAD@"
+	db "SIGEWEG@"
 DiglettsCaveName: ; 7166c (1c:566c)
-	db "DIGLETT's CAVE@"
+	db "DICANas HYDELS@"
 RocketHQName: ; 7167a (1c:567a)
-	db "ROCKET HQ@"
+	db "ROCCASELD@"
 SilphCoName: ; 71684 (1c:5684)
-	db "SILPH CO.@"
+	db "SYLF GEGYLD@"
 PokemonMansionName: ; 7168e (1c:568e)
-	db $4a," MANSION@"
+	db $4a," BEORSELD@"
 SafariZoneName: ; 71698 (1c:5698)
-	db "SAFARI ZONE@"
+	db "FARUFELD@"
 CeruleanCaveName: ; 716a4 (1c:56a4)
-	db "CERULEAN CAVE@"
+	db "BLEOHYDELS@"
 PowerPlantName: ; 716b2 (1c:56b2)
-	db "POWER PLANT@"
+	db "ÞUNORWEORC@"
